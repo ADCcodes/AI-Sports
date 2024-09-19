a47efdf08092a2d7bbdcc4b5e411c3004659e8c3
Andres2803 <94024580+ADCcodes@users.noreply.github.com>
2024-09-18 18:24:22 -0600

    Cambios AI-NFL

--- src/Fantasy/NFL/src/AI-NFL-Fantasy.py
+++
@@ -1,5 +0,0 @@
-import nfl_data_py as nfl
-nfl.import_pbp_data(2023, 1, downcast=True, cache=False, alt_path=None)
-
-# Fetch the PBP data for the first game of the 2023 season
-pbp = nfl.games[2023][1]
